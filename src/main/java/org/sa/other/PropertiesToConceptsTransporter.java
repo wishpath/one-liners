e6681aa9c5c12f3776a1948810d35bb8abb8709b
Simas <simonas.aruna@gmail.com>
2025-06-03 11:01:21 +0300

    master: sorted .concepts files

--- src/main/java/org/sa/other/PropertiesToConceptsTransporter.java
+++ src/main/java/org/sa/other/PropertiesToConceptsTransporter.java
@@ -5,8 +5,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Map;
-import java.util.Properties;
+import java.util.*;
 
 public class PropertiesToConceptsTransporter {
   private static final Path TOPICS = Paths.get("src/main/java/org/sa/concepts/topics");
@@ -22,12 +21,19 @@ public class PropertiesToConceptsTransporter {
   private static void transport(Path path) throws IOException {
     for (Path p : Files.walk(path).filter(p -> p.toString().endsWith(".properties")).toList()) {
       Properties props = new Properties();
-      props.load(Files.newInputStream(p));
+
+      //props.load(Files.newInputStream(p)); //was replaced to solve correct lithuanian letters
+      try (var reader = Files.newBufferedReader(p, java.nio.charset.StandardCharsets.UTF_8)) {
+        props.load(reader);
+      }
 
       Path newPath = p.getParent().resolve(p.getFileName().toString().replaceAll("\\.properties$", ".concepts"));
       BufferedWriter writer = Files.newBufferedWriter(newPath);
 
-      for (Map.Entry<Object, Object> e : props.entrySet())
+      List<Map.Entry<Object, Object>> sortedList = new ArrayList<>(props.entrySet());
+      Collections.sort(sortedList, Comparator.comparing(e -> e.getKey().toString()));
+
+      for (Map.Entry<Object, Object> e : sortedList)
         writer.write(e.getKey() + "=" + e.getValue() + "\n");
 
       writer.flush();
