05d98dcc01217b37b568bdee547a48064f944407
Simas <simonas.aruna@gmail.com>
2025-12-09 21:29:02 +0200

    master: notes

--- src/main/java/org/sa/service/ConceptsLoader.java
+++ src/main/java/org/sa/service/ConceptsLoader.java
@@ -19,7 +19,6 @@ public class ConceptsLoader {
   public Map<String, ConceptDTO> key_concept = loadConceptsWithAttributes();
   public final TreeMap<Integer, Set<String>> score_keySet = mapScores(key_concept);
 
-
   public static TreeMap<Integer, Set<String>> mapScores(Map<String, ConceptDTO> key_concept) {
     TreeMap<Integer, Set<String>> score_keyList = new TreeMap<>(); //auto ascending map
     for (ConceptDTO c : key_concept.values()) score_keyList.computeIfAbsent(c.score, k -> new HashSet<>()).add(c.key);
